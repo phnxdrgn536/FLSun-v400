44dafce0dc99f40aebb95f03a4c8d81d98eca71f
phnxdrgn536 <phnxdrgn536@gmail.com>
2023-01-12 15:10:29 -0500

    Added CPL project code

---
+++ CPL Project/Cura Profile Loader/Form1.cs
@@ -0,0 +1,38 @@
+namespace Cura_Profile_Loader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void load_pb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cura_v_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void firm_v_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+      
+    }
+
+}
